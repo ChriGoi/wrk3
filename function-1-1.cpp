871aa9eb59441b9be1f72fe9525c1950d898b62c
Christian <christian.goiak@outlook.com>
2023-03-15 13:40:59 +1030

    jhj

---
+++ function-1-1.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void size_of_variable_star_t(t *)
+{
+    p = &t;
+    return;
+    printf("%d", p)
+}
---
+++ main-1-1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <function-1-1.cpp>
+
+int main()
+{
+    t *= 3;
+    void int size_of_variable_star_t(t *);
+    size_of_variable_star_t(t *);
+    return 0;
+}
